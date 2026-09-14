5f8e1600f5b8bf0cde740ed3f48c739e59a3dbb5
Yuri Domingues <yuridomingues.contato@gmail.com>
2026-09-14 12:35:58 -0300

    add mary-had-a-little-lamb/mary-had-a-little-lamb.ino

---
+++ mary-had-a-little-lamb/mary-had-a-little-lamb.ino
@@ -0,0 +1,42 @@
+const int BUZZER_PIN = 8;
+
+const int melody[] = {
+  330, 294, 262, 294,
+  330, 330, 330,
+  294, 294, 294,
+  330, 392, 392,
+  330, 294, 262, 294,
+  330, 330, 330, 330,
+  294, 294, 330, 294, 262
+};
+
+const int beats[] = {
+  4, 4, 4, 4,
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4, 1
+};
+
+const int TEMPO = 110;
+
+void playSong() {
+  const int wholeNote = (60000 * 4) / TEMPO;
+  const int noteCount = sizeof(melody) / sizeof(melody[0]);
+
+  for (int i = 0; i < noteCount; i++) {
+    int duration = wholeNote / beats[i];
+    tone(BUZZER_PIN, melody[i], duration * 0.9);
+    delay(duration);
+    noTone(BUZZER_PIN);
+  }
+}
+
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+  playSong();
+}
+
+void loop() {}
